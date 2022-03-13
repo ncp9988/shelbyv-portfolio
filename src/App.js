37996a7bdf521fb01eeab38a804e783e701994c3
Chau Pham <ncp9988@gmail.com>
2022-03-13 10:16:34 -0500

    create contact component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import About from './components/About';
 import Navigation from './components/Navigation';
+import Project from './components/Project';
+import Contact from './components/Contact';
 
 function App() {
-
+  const [projectSelected, setProjectSelected] = useState(false)
+  const [contactSelected, setContactSelected] = useState(false);
   return (
     <div>
+      <Navigation>
+        projectSelected={projectSelected}
+        setProjectSelected={setProjectSelected}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      </Navigation>
       <main>
-        <Navigation></Navigation>
         <About></About>
+        <Project></Project>
+        <Contact></Contact>
       </main>
     </div>
   );
