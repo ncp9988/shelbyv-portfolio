781f228b421008b5f5bdff97b127651b41cd6a5b
Chau Pham <ncp9988@gmail.com>
2022-03-12 20:51:12 -0600

    setting about componet

---
+++ src/components/About/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function About() {
+  return (
+    <section>
+      <h1 id="about">About me</h1>
+      <p> Dcbsjdcsdjc dcsdjcs dcbjscj dhsd dhcbsj</p>
+    </section>
+  );
+}
+
+export default About;
