bbe71a79840296d0e57e5a7588861ec0f188583a
Chau Pham <ncp9988@gmail.com>
2022-03-13 09:40:58 -0500

    create project component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import About from './components/About';
+import Navigation from './components/Navigation';
 
 function App() {
 
   return (
     <div>
       <main>
+        <Navigation></Navigation>
         <About></About>
       </main>
     </div>
---
+++ src/components/Project/index.js
@@ -0,0 +1,82 @@
+import React from 'react';
+
+
+function Project() {
+    return (
+        <section id="works">
+            <h2>Works</h2>
+            <section id="portfolio">
+                <div class="container1">
+
+                    <div class="imgcontainer">
+                        <a href="https://github.com/ncp9988/myfirstfanpage.git">
+
+
+                            <img class="photo" src="../../assets/projects/fanpage.jpeg" alt="Fan Page" /> </a>
+
+                    </div>
+                    <div class="pic">
+
+
+                        <h3>First fanpage</h3>
+                    </div>
+                </div>
+                <div class="container1">
+                    <div class="imgcontainer">
+                        <a href="https://github.com/ncp9988/run-buddy.git">
+
+
+                            <img class="photo" src="../../assets/projects/hero-bg.jpg" alt="Run-Buddy" /></a>
+                    </div>
+                    <div class="pic">
+                        <h3>Run-Buddy</h3>
+                    </div>
+                </div>
+
+                <div class="container1">
+
+                    <div class="imgcontainer">
+
+                        <a href="https://github.com/ncp9988/coderefactor.git">
+                            <img class="photo" src="../../assets/projects/digital-marketing-meeting.jpg" alt="Code Refactor" />
+                        </a>
+
+                    </div>
+                    <div class="pic">
+                        <h3>Code Refactor</h3>
+                    </div>
+                </div>
+                <div class="container1">
+
+                    <div class="imgcontainer">
+
+                        <a href="https://github.com/ncp9988/Eat-Eat-Eat.git">
+                            <img class="photo" src="../../assets/projects/eat.png" alt="Eat-Eat-Eat" />
+                        </a>
+
+                    </div>
+                    <div class="pic">
+                        <h3>Eat-Eat-Eat</h3>
+                    </div>
+                </div>
+                <div class="container1">
+
+                    <div class="imgcontainer">
+
+                        <a href="https://github.com/Arcanaut/Group-Project-2.git">
+                            <img class="photo" src="../../assets/projects/vFriend.png" alt="vFriend" />
+                        </a>
+
+                    </div>
+                    <div class="pic">
+                        <h3>vFriend</h3>
+                    </div>
+                </div>
+
+
+
+            </section>
+        </section>
+    );
+}
+export default Project;
