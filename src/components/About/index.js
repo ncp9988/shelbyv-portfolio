c6cdc13cbfba2aa08dd4c7b35126d0e26bd8c33f
Chau Pham <ncp9988@gmail.com>
2022-03-13 21:38:21 -0500

    deploy to github page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,7 +4,9 @@ function About() {
     <section>
       <h2 id="about">About me</h2>
       <p> Hi, It's Shelby. I graduated from Dallas Community College 2019 in major Associate of Science. Along with that I am also an Esthetician as a part time job, so far it has been more than
-                3 years. I'm enjoying Coding Boot Camp Program from Southern Methodist University CAPE and cannot wait to experience it in real world.   </p>
+        3 years.
+        I'm enjoying Coding Boot Camp Program from Southern Methodist University CAPE and cannot wait to experience it in real world.
+      </p>
     </section>
   );
 }
