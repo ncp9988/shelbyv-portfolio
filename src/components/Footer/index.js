250aecc44ba4293f91e4162866b0a654c79b9ee3
Chau Pham <ncp9988@gmail.com>
2022-03-13 21:10:39 -0500

    update readme file

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,8 @@ function Footer() {
   return (
     <footer id="footer">
       <h3>Copyright &copy; Ngoc Chau Pham 2022</h3>
-      <a href="https://github.com/ncp9988"><i class="fab-brands fab-github"></i></a>
+      <a href="https://github.com/ncp9988">GitHub<i class="fab-brands fab-github"></i></a>
+      
     </footer>
   );
 }
