c4a799758e547487a30a8a954f904927e4f8b72a
Chau Pham <ncp9988@gmail.com>
2022-03-13 21:22:45 -0500

    added github icon

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { AiFillGithub } from "react-icons/ai";
+
 function Footer() {
   return (
     <footer id="footer">
       <h3>Copyright &copy; Ngoc Chau Pham 2022</h3>
-      <a href="https://github.com/ncp9988">GitHub<i class="fab-brands fab-github"></i></a>
+      <a href="https://github.com/ncp9988"><AiFillGithub /></a>
       
     </footer>
   );
